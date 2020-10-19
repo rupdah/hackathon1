395fc6919ddcff6a6563f264fd13d0425721d970
rupdah <rupeshdahuja@gmail.com>
2020-10-19 13:52:22 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 12:17:26 2020
+
+@author: Vikram.V.Kumar
+"""
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import pydeck as pdk
+from PIL import Image
+#image = Image.open('C:\\Users\Vikram.V.Kumar\Desktop\Advanced Analytics Practice\Hackathon\Data\Shell_Image.jpg')
+#st.image(image)
+
+
+
+
+
+
+st.subheader('Map of electric vehicle charging Stations with Electric vehicle density')
